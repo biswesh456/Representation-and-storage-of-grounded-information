f44c82e9ceeb9d1af201fdf73ab5043580b81aba
labicquette <komete17@gmail.com>
2025-01-04 12:56:42 +0100

    Initialization with messages and cleanup

--- source/memgpt.py
+++ source/memgpt.py
@@ -9,6 +9,7 @@ import os
 import memgpt
 from time import sleep
 os.environ['HF_HOME'] = '/data/almanach/user/cdearauj/models/'
+#os.environ['HF_HOME'] = '/data/clustext/user/bmohapat/huggingface/'
 
 base_url = "http://localhost:8283"
 
@@ -53,56 +54,30 @@ for d in directories:
     preprompts, prompts, queries = prompting.load_prompt(files, tokenizer=None, model_id=None, processing="memgpt", CoT=False, dataset_name=None)
     messages, times, users, answer, answer_user, answer_time = prompting.load_prompt(files, tokenizer=None, model_id=None, processing="memgpt-messages", CoT=False, dataset_name=None)
     for i,f in enumerate(files):
-
         if users[i][-1] == "A":
             users[i] = list(map(lambda x: x.replace('A', 'user').replace('B', 'assistant'), users[i]))
         else :
             users[i] = list(map(lambda x: x.replace('B', 'user').replace('A', 'assistant'), users[i]))
-        agent = client.create_agent(
+        agent_state = client.create_agent(
             name=d.split("/")[-1] + "_"+f.split("/")[-1].split(".")[0], 
             memory = ChatMemory(
-            persona= preprompts[i],
-            human="I'm a default user"
-        ),
-            # Actual try to put the messages in the memgpt
-            initial_message_sequence= [ {"role":users[i][j], "content":m} for j,m in enumerate(messages[i])]
+                persona= preprompts[i],
+                human="I'm a default user"
+            ),
+            #TODO PUT HOURS FROM DIALOG  IN MEMGPT MESSAGES METADATA
+            initial_message_sequence = [ {"role":users[i][j], "text":m, "user_id":users[i][j]} for j,m in enumerate(messages[i][:-1])]
         )
-        print("agent_messages : ",agent._messages)
-        """
-        Message.dict_to_message(
-            agent_id=self.agent_state.id,
-            user_id=self.agent_state.user_id,
-            model=self.model,
-            openai_message_dict=openai_message_dict,
-            # created_at=timestamp,
-        )
-        """
         
-        #TODO START BY MESSAGE[0]  PUT MESSAGES IN AGENT.MESSAGES, MODIFY PROMPT WITH USER/AGENT INSTEAD OF USER A/B, each utterance is a message
-        #TODO PUT HOURS FROM DIALOG  IN MEMGPT MESSAGES METADATA
-        response = client.user_message(
-            agent_id=agent.id, 
-            message=prompts[i],
-            include_full_message=False
-        )
-        sleep(30)
-        print("1 : ", response)
         
-        response = client.user_message(
-            agent_id=agent.id, 
-            message= queries[-1], 
-            include_full_message=False
-        )
-        print("2 : ", response)
-        response = client.user_message(
-            agent_id=agent.id, 
-            message= "/memory", 
-            include_full_message=False
-        )
-        print("3 : ", response)
+        
 
-        
-    #print([c.id for c in client.list_agents()])
+        #TODO How do we get the answer from the model ?
+        response = client.user_message(
+            agent_id=agent_state.id,
+            message= messages[i][-1], #last message is the query
+            include_full_message=True #TODO true or not true ?
+        )
+
 
 print("MEMGPT")
 #client.delete_agent("basic_agent")
@@ -113,4 +88,3 @@ print([c.id for c in client.list_agents()])
 
 for a in all_agents:
     client.delete_agent(a)
-                        
